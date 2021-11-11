6422b1ee4d30fc2f1bf92257e479faf2d6332810
Fujimoto Seiji <fujimoto@ceptord.net>
2021-11-11 22:41:18 +0900

    buffer

--- ThinBridge/ThinBridge.cpp
+++ ThinBridge/ThinBridge.cpp
@@ -1726,7 +1726,7 @@ void CCRre::IEStart(CString& strURL)
 	HSZ hszService={0};
 	HSZ hszTopic={0};
 	TCHAR buf[1024];
-	_tcscpy_s(buf, 1024, (LPTSTR)strURL);
+	_tcscpy_s(buf, 1024, (LPCSTR)(LPCTSTR)strURL);
 
 	HCONV hConv={0};
 	HDDEDATA hDDEData={0};
