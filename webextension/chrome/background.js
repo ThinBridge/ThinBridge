4d6a564bf50c9e691451ca5c13701b0492af3ab9
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2022-08-31 18:47:10 +0900

    Suppress too much logs

--- webextension/chrome/background.js
+++ webextension/chrome/background.js
@@ -172,7 +172,7 @@ const ThinBridgeTalkClient = {
   match(section, url, namedSections) {
     for (const name of section.ExcludeGroups) {
       const foreignSection = namedSections[name.toLowerCase()];
-      console.log(`* Referring exlude group ${name}: ${JSON.stringify(foreignSection && foreignSection.Patterns)}`);
+      //console.log(`* Referring exlude group ${name}: ${JSON.stringify(foreignSection && foreignSection.Patterns)}`);
       if (!foreignSection)
         continue;
       for (const pattern of foreignSection.Patterns) {
