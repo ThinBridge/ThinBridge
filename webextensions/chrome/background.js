f2ea8d930daa3b0574ffe61692ebc1373657c06c
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2024-05-15 14:18:54 +0900

    Don't close tab if not allowed

--- webextensions/chrome/background.js
+++ webextensions/chrome/background.js
@@ -165,6 +165,9 @@ const ThinBridgeTalkClient = {
       const query = new String('Q ' + BROWSER + ' ' + url);
       chrome.runtime.sendNativeMessage(SERVER_NAME, query);
 
+      if (!closeTab)
+        return;
+
       let existingTab = tab;
       let counter = 0;
       do {
